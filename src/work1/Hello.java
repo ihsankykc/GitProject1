95cfa02f3a84f7c2e8a052e25c6f9bad462b9e36
ihsankykc <ihsankykc@gmail.com>
2021-10-03 20:15:52 +0200

    added more work

--- src/work1/Hello.java
+++ src/work1/Hello.java
@@ -5,5 +5,6 @@ public class Hello {
         System.out.println("Work1 started");
         System.out.println("created Git repository");
         System.out.println("Made few commit");
+        System.out.println("Github repo is already published");
     }
 }
